7b3fec10f7cd45e990a98309fac66d545232d4f8
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-01-31 10:30:21 +0100

    WOBS-2957: Adds Solr controllers

---
+++ Controller/OmnitickerController.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2012 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\SolrSearchPluginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class OmnitickerController extends Controller
+{
+    /**
+     * @var array
+     */
+    private $sources = array(
+        'tageswoche' => array('news', 'dossier', 'blog'),
+        'twitter' => 'tweet',
+        'agentur' => 'newswire',
+        'link' => 'link',
+        'en' => array('newswire'),
+    );
+
+    /**
+     * @Route("/omniticker")
+     */
+    public function omnitickerAction(Request $request)
+    {
+        if ($this->container->get('webcode')->findArticleByWebcode($request->query->get('q')) !== null) {
+
+            $this->_helper->redirector->setCode(302);
+            $this->_helper->redirector->gotoRoute(array(
+                'webcode' => $request->get('q'),
+            ), 'webcode', false, false);
+        }
+
+        $queryService = $this->container->get('newscoop_solrsearch_plugin.query_service');
+        $parameters = $request->query->all();
+
+        if (array_key_exists('format', $parameters)) {
+
+            $solrParameters = $this->encodeParameters($parameters);
+            $response = $queryService->find($solrParameters);
+        } else {
+
+            $templatesService = $this->container->get('newscoop.templates.service');
+
+            $response = new Response();
+            $smarty = $templatesService->getSmarty();
+            $response->setContent($templatesService->fetchTemplate("search.tpl"));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Build solr params array
+     *
+     * @return array
+     */
+    protected function encodeParameters(array $parameters)
+    {
+        $queryService = $this->container->get('newscoop_solrsearch_plugin.query_service');
+
+        return array_merge($queryService->encodeParameters($parameters), array(
+            'q' => '*:*',
+            'fq' => implode(' AND ', array_filter(array(
+                $this->buildSolrSectionParam($parameters),
+                $this->buildSolrSourceParam($parameters),
+                $queryService->buildSolrDateParam($parameters),
+                (array_key_exists('source', $parameters) && $parameters['source'] === 'en') ? 'section:swissinfo' : null,
+                '-switches:print',
+            ))),
+            'sort' => 'published desc',
+            'spellcheck' => 'false',
+        ));
+    }
+
+    /**
+     * Build solr source filter
+     *
+     * @return string
+     */
+    private function buildSolrSourceParam($parameters)
+    {
+        $source = (array_key_exists('source', $parameters)) ? $parameters['source'] : null;
+
+        if (!empty($source) && array_key_exists($source, $this->sources)) {
+            $sources = (array) $this->sources[$source];
+        } else {
+            $sources = array();
+            foreach ($this->sources as $types) {
+                $sources = array_merge($sources, (array) $types);
+            }
+        }
+
+        return sprintf('type:(%s)', implode(' OR ', array_unique($sources)));
+    }
+
+    /**
+     * Build solr section filter
+     *
+     * @return string
+     */
+    private function buildSolrSectionParam($parameters)
+    {
+        $section = (array_key_exists('section', $parameters)) ? $parameters['section'] : null;
+        if ($section !== null) {
+            return sprintf('section:("%s")', json_encode($section));
+        }
+    }
+}
---
+++ Controller/SearchController.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @package Newscoop
+ * @copyright 2012 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\OmnitickerPluginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Newscoop\Http\Client;
+
+class SearchController extends Controller
+{
+    /**
+     * @Route("/search")
+     */
+    public function searchAction(Request $request)
+    {
+        if ($this->container->get('webcode')->findArticleByWebcode($request->query->get('q')) !== null) {
+
+            return $this->redirect(
+                sprintf('/%s', $request->get('q')), 302
+            );
+        }
+
+        $queryService = $this->container->get('newscoop_solrsearch_plugin.query_service');
+        $parameters = $request->query->all();
+
+        if (array_key_exists('format', $parameters)) {
+
+            $solrParameters = $this->encodeParameters($parameters);
+            $response = $queryService->find($solrParameters);
+        } else {
+
+            $templatesService = $this->container->get('newscoop.templates.service');
+
+            $response = new Response();
+            $smarty = $templatesService->getSmarty();
+            $response->setContent($templatesService->fetchTemplate("search.tpl"));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Build solr params array
+     *
+     * @return array
+     */
+    protected function encodeParameters(array $parameters)
+    {
+        $queryService = $this->container->get('newscoop_solrsearch_plugin.query_service');
+
+        $fq = implode(' AND ', array_filter(array(
+            $this->buildSolrTypeParam($parameters),
+            $queryService->buildSolrDateParam($parameters),
+            '-section:swissinfo', // filter en news
+        )));
+
+        return array_merge($queryService->encodeParameters($parameters), array(
+            'q' => $this->buildSolrQuery($parameters),
+            'fq' => empty($fq) ? '' : "{!tag=t}$fq",
+            'sort' => $parameters['sort'] === 'latest' ? 'published desc' : ($parameters['sort'] === 'oldest' ? 'published asc' : 'score desc'),
+            'facet' => 'true',
+            'facet.field' => '{!ex=t}type',
+            'spellcheck' => 'true',
+        ));
+    }
+
+    /**
+     * Build solr query
+     *
+     * @return string
+     */
+    private function buildSolrQuery($parameters)
+    {
+        $q = (array_key_exists('q', $parameters)) ? trim($parameters['q']) : sha1(__FILE__); // search for nonsense to show empty search result page
+
+        if ($this->container->get('webcode')->findArticleByWebcode($q) !== null) {
+            return sprintf('webcode:\%s', $q);
+        }
+
+        $matches = array();
+        if (preg_match('/^(author|topic):([^"]+)$/', $q, $matches)) {
+            $q = sprintf('%s:"%s"', $matches[1], json_encode($matches[2]));
+        }
+
+        return $q;
+    }
+
+    /**
+     * Build solr type param
+     *
+     * @return string
+     */
+    private function buildSolrTypeParam($parameters)
+    {
+        if (!array_key_exists('type', $parameters) || !array_key_exists($parameters['type'], $this->types)) {
+            return;
+        }
+
+        $type = $parameters['type'];
+
+        return sprintf('type:(%s)', is_array($this->types[$type]) ? implode(' OR ', $this->types[$type]) : $this->types[$type]);
+    }
+
+    public function errorAction()
+    {
+        $this->getResponse()->setHttpResponseCode(503);
+    }
+}
